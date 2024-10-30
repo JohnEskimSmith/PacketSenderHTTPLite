9c9dee6ec31599840c3d115b9cc8e56533739c71
john.eskim.smith <john.eskim.smith@gmail.com>
2024-10-30 23:35:18 +0300

    decode bytes

--- lib/util/io.py
+++ lib/util/io.py
@@ -101,7 +101,7 @@ async def write_to_stdout(io, record: str):
     """
     Write in 'wb' mode to io, input string in utf-8
     """
-    return await io.write(record.encode("utf-8") + b"\n")
+    return await io.write(record + "\n")
 
 
 async def write_to_file(io, record: str):
